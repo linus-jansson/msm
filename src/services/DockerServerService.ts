50886233305e0462ad60f18c9020b9ae37744d6c
Jansson Linus (Utvecklare) <linus.jansson2@uppsala.se>
2023-04-12 15:30:49 +0200

    added start command

--- src/services/DockerServerService.ts
+++ src/services/DockerServerService.ts
@@ -9,13 +9,18 @@ import randomName from '@/lib/helpers/randomname';
 import * as fs from 'fs'
 import { exec } from 'child_process';
 
+const findFreePort = () => "25565";
+
 /* https://github.com/itzg/docker-minecraft-server/blob/master/README.md */
 @Injectable()
 export class DockerServerService {
+    servers_path = process.cwd() + "/servers/";
     constructor(
         private prisma: PrismaService,
         private readonly mojang: MojangService
-    ) {}
+    ) {
+        console.log(this.servers_path)
+    }
 
     async ExecuteCommand(command: string): Promise<string> {
         return new Promise((resolve, reject) => {
@@ -33,48 +38,15 @@ export class DockerServerService {
         _type?: string,
         should_start_after_creation: boolean = false
     ): Promise<object> {
-        console.log(targetVersion, _type);
         const new_tag = randomName();
         const new_server_version = targetVersion || await this.mojang.getLatestVersion();
         const new_server_type = _type || "vanilla";
+        const port = findFreePort();
 
-        const servers_path = process.cwd();
-        console.log(servers_path)
-        const path = `${servers_path}/servers/${new_tag}/`;
-        console.log(path)
 
-        const docker_compose = `
-        version: "3"
-
-        services:
-            "${new_tag}":
-                image: itzg/minecraft-server
-                ports:
-                    - 25565:25565
-                environment:
-                    - EULA="TRUE"
-                    - VERSION="${new_server_version}"
-                tty: true
-                stdin_open: true
-                restart: unless-stopped
-                volumes:
-                    # attach a directory relative to the directory containing this compose file
-                    - ./server_data:/data
-        `;
-        // Create server folder
-        fs.mkdirSync(path, {recursive: true});
-        // Create server_data folder
-        fs.mkdirSync(`${path}/server_data`, {recursive: true});
-        // Create docker-compose.yml
-        fs.writeFileSync(`${path}/docker-compose.yml`, docker_compose);
-
-        if (should_start_after_creation)
-        {
-            await this.ExecuteCommand(`cd ${path} && docker-compose up -d`);
-        }
-
-        // Create docker ymal to server folder location <servers>/<tag>/docker-compose.yml
         /*
+            Create docker ymal to server folder location <servers>/<tag>/docker-compose.yml
+            file structure:
             <servers>
                 <tag>
                     docker-compose.yml
@@ -84,6 +56,36 @@ export class DockerServerService {
                         eula.txt
                         ....
         */
+
+        const new_server_path = `${this.servers_path}/${new_tag}/`;
+
+        const docker_compose = `version: "3"
+
+services:
+    "${new_tag}":
+        image: itzg/minecraft-server
+        environment:
+            - EULA="TRUE"
+            - VERSION="${new_server_version}"
+        tty: true
+        stdin_open: true
+        restart: unless-stopped
+        volumes:
+            # attach a directory relative to the directory containing this compose file
+            - ./server_data:/data
+        `;
+        // Create server folder
+        fs.mkdirSync(new_server_path, {recursive: true});
+        // Create server_data folder
+        fs.mkdirSync(`${new_server_path}/server_data`, {recursive: true});
+        // Create docker-compose.yml
+        fs.writeFileSync(`${new_server_path}/docker-compose.yml`, docker_compose);
+
+        if (should_start_after_creation)
+        {
+            await this.ExecuteCommand(`cd ${new_server_path} && docker-compose up -d`);
+        }
+
 
         
 
@@ -99,10 +101,29 @@ export class DockerServerService {
         // console.log(res);
 
         return {
-            ...db_result,
+            server: {
+                ...db_result,
+            },
+            custom_properties: {},
             will_start: should_start_after_creation,
-            server_port: 25565,
+            server_port: (should_start_after_creation === true) ? findFreePort() : null, // when a server is started start searching for a free port on 25565 and increment by 1
         };
+    }
+
+    async Start(tag: string): Promise<{status:string, port: string} | {status: string, error: any}> {
+        const port = findFreePort();
+        try {
+            await this.ExecuteCommand(`cd ${this.servers_path}/${tag} && docker-compose up -p ${port}:25565 -d`);
+            return {
+                status: "success",
+                port: port 
+            }
+        } catch (error) {
+            return {
+                status: "Failed",
+                error: error,
+            }
+        }
     }
 
 }
