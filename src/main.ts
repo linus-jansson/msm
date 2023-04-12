4aab8a0e553954cf0f936105e8184de829c47cbb
Jansson Linus (Utvecklare) <linus.jansson2@uppsala.se>
2023-04-12 15:52:31 +0200

    Added swagger api documentation

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { PrismaService } from '@/services/PrismaService';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const _prismaService = app.get(PrismaService);
-  await _prismaService.enableShutdownHooks(app)
+    const app = await NestFactory.create(AppModule);
+    const _prismaService = app.get(PrismaService);
+    await _prismaService.enableShutdownHooks(app)
+
+    const config = new DocumentBuilder()
+    .setTitle('Msm API')
+    .setDescription('The msm API description')
+    .addTag('msmapi')
+    .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
